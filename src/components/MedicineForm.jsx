133546ad328f805f8a09861f1769678261c31196
William Gomes <williamjog@hotmail.com>
2021-08-15 10:50:43 -0300

    Remove espaços

--- src/components/MedicineForm.jsx
+++ src/components/MedicineForm.jsx
@@ -10,20 +10,14 @@ const MedicineForm = () => {
     setPrice, setStock, isEditing } = useContext(MedicinesContext);
 
   const styles = makeStyles({
-    root: {
-      minWidth: 275,
-    },
+    root: { minWidth: 275 },
     bullet: {
       display: 'inline-block',
       margin: '0 2px',
       transform: 'scale(0.8)',
     },
-    title: {
-      fontSize: 14,
-    },
-    pos: {
-      marginBottom: 12,
-    },
+    title: { fontSize: 14 },
+    pos: { marginBottom: 12 },
   });
 
   const classes = styles();
