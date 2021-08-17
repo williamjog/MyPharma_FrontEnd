42573eafe53fe8651a7a27a514d5740e6314ad3c
William Gomes <williamjog@hotmail.com>
2021-08-16 23:30:34 -0300

    Corrige estoque maximo em branco

--- src/components/FilterButtons.jsx
+++ src/components/FilterButtons.jsx
@@ -7,21 +7,20 @@ import axios from 'axios';
 import '../style/App.css';
 
 const FilterButtons = () => {
-  const { products, setProducts, minimumValue, maximumValue, setMaximumValue,
-    setIsEditing, setCod, setName, setDescription, setPrice, setStock } = useContext(ProductContext);
+  const { products, setProducts, minimumValue, 
+          maximumValue, setIsEditing, setCod, 
+          setName, setDescription, setPrice, setStock } = useContext(ProductContext);
 
   const filterProducts = () => {
-    if (minimumValue < 0 || maximumValue <= 0 || !minimumValue || !maximumValue) {
+    if (maximumValue && (minimumValue < 0 || maximumValue <= 0 || !minimumValue || !maximumValue)) {
       return alert('Não há valores negativos para estoque.');
     }
-    if (!maximumValue) {
-      setMaximumValue(0);
-    }
-    if (Number(minimumValue) > Number(maximumValue)) {
+    if (maximumValue && Number(minimumValue) > Number(maximumValue)) {
       return alert('O valor mínimo precisa ser menor que o máximo');
     }
     const filteredMedicines = products.filter((product) => {
-      return product.estoque >= minimumValue && product.estoque <= maximumValue;
+      return maximumValue ? product.estoque >= minimumValue && product.estoque <= maximumValue :
+      product.estoque >= minimumValue;
     });
     setProducts(filteredMedicines);
   }
